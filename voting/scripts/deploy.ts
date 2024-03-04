e1e87a25fccc031fd5d087f24fc5c56b20a915f8
caryllfranz <142728727+caryllfranz@users.noreply.github.com>
2024-03-04 16:07:38 +0000

    tryingvotingapp

---
+++ voting/scripts/deploy.ts
@@ -0,0 +1,18 @@
+const { ethers } = require("ethers");
+
+async function main() {
+  const Voting = await ethers.getContractFactory("Voting");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const Voting_ = await Voting.deploy(["caryll", "lebron", "Topson", "sumail"], 90);
+  console.log("Contract address:", Voting_.address);
+
+
+}
+
+main()
+ .then(() => process.exit(0))
+ .catch(error => {
+   console.error(error);
+   process.exit(1);
+ });
